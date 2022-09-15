d2237586e29e5d8d2b8edee5d58ddcfb9c934dcd
Becky <temmyisijola@gmail.com>
2022-09-15 09:51:07 -0700

    added task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: returns the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,5 @@ int _putchar(char c);
 int _isupper(int ch);
 
 int _isdigit(int c);
+
+int mul(int a, int b);
