af5f10c3ad6d7b70a1045049f8d5a1afa62ed2fd
Becky Isijola <temmyisijola@gmail.com>
2022-09-24 13:55:33 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tmp;
+	int i, j, k;
 
 	j = n - 1;
 
 	for (i = 0; i < n / 2; i++)
 	{
-		tmp = a[i];
+		k = a[i];
 		a[i] = a[j];
-		a[j--] = tmp;
+		a[j--] = k;
 	}
 }
