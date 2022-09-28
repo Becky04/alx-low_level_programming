b816b65cbfd041b143883a609490f828558bf81c
Becky <temmyisijola@gmail.com>
2022-09-28 12:21:25 -0700

    fuction that returns the factorial of a given number

---
+++ 0x08-recursion/3-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = factorial(1);
+	printf("%d\n", r);
+	r = factorial(5);
+	printf("%d\n", r);
+	r = factorial(10);
+	printf("%d\n", r);
+	r = factorial(-1024);
+	printf("%d\n", r);
+
+	return (0);
+}
