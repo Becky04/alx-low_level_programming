be8d224a3f5701cf30595b66fef0587a7ed84479
Becky Isijola <temmyisijola@gmail.com>
2022-09-29 14:36:07 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed
- * by a new line, to stdout.
+ * _puts - write a function that prints a string, followed
+ * by a new line to stdout
  * @str: an input string
- *
- * Return: Nothing
  */
 void _puts(char *str)
 {
