324aee5c86d5d32daa0b1b5fad2a867c61701834
Becky <temmyisijola@gmail.com>
2022-10-04 09:03:50 -0700

    unction returns a pointer to a duplicated string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates string
+ * @str: string to duplicate
+ *
+ * Return: a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *newString;
+	unsigned int size;
+
+	size = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		size++;
+	newString = (char *) malloc(sizeof(char) * (size + 1));
+	if (str == NULL || newString == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		newString[i] = str[i];
+	newString[size] = '\0';
+	return (newString);
+}
