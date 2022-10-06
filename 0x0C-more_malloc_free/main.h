152f61a26fe6a131637f21e4942d45e84d6940ea
Becky <temmyisijola@gmail.com>
2022-10-06 14:45:43 -0700

    added header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,13 +2,13 @@
 #define MAIN_H
 
 int _putchar(char c);
-void *malloc(size_t size);
-void free(void *ptr);
-void exit(int status);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
-#endif /* MAIN_H */
+#endif
