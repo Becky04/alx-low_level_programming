0125a3bfe767d7c29bac81696eb9ddbd5835ba95
Becky <temmyisijola@gmail.com>
2022-10-24 15:39:04 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  *
  * Return: NULL if the function fails
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int id, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *copy = *head;
 	unsigned int node;
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int id, int n)
 	if (idx = 0)
 	{
 		new->next = copy;
-		*hed =new;
+		*head =new;
 		return (new);
 	}
 	for (node = 0; node < (idx - 1); node++)
