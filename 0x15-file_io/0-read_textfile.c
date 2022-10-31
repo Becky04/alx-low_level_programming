b2c75df5f37d0d95d4db8d6638c83a732dd9065d
Becky <temmyisijola@gmail.com>
2022-10-31 08:28:27 -0700

    FILE IO

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - reads a textfile and prints the contents
+ * to POSIX STDOUT
+ * @filename: name of the file to read from
+ * @letters: number of characters to print to STDOUT
+ *
+ * Return: number of characters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fild = 0, chk = 0;
+	char *buffer;
+
+	if (!filename || !letters)
+		return (0);
+
+	fild = open(filename, O_RDONLY);
+	if (fild < 0)
+		return (0);
+	buffer = malloc(sizeof(char) * (letters));
+	chk = read(fild, buffer, letters);
+	chk = read(STDOUT_FILENO, buffer, chk);
+
+	if (chk < 0)
+		return (0);
+	close(fild);
+	free(buffer);
+	return (chk);
+
+
+}
